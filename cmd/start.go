aa4b81c85caff84f4f6314326525bb8e3799a7f2
Ollie Phillips <ollie@eantics.co.uk>
2016-04-08 17:25:39 +0100

    Removed a debug line

--- cmd/start.go
+++ cmd/start.go
@@ -24,7 +24,6 @@
 package cmd
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -58,7 +57,7 @@ func createConfigToml() error {
 func createMarkdownTemplate(template string, filename string, isPage bool) error {
 	themePath := basePath + string(filepath.Separator) + "sites" + string(filepath.Separator) + domain + string(filepath.Separator) + "theme" + string(filepath.Separator) + theme
 	sitePath := basePath + string(filepath.Separator) + "sites" + string(filepath.Separator) + domain
-	fmt.Println(themePath)
+
 	if !dirExist(themePath) {
 		log.Fatal("Error cannot find theme to create markdown templates")
 	}
